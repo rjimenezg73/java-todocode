9099defcce038a5584dad8139644fcce4d27661c
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-18 22:29:31 -0600

    Prueba Capas - Interfaz Grafica

---
+++ PruebaCapas/src/logica/PruebaCapas.java
@@ -0,0 +1,15 @@
+package logica;
+
+import igu.Pantalla;
+
+
+public class PruebaCapas {
+
+  public static void main(String[] args) {
+    
+    Pantalla panta = new Pantalla();
+    panta.setVisible(true);
+    panta.setLocationRelativeTo(null);
+  }
+  
+}
