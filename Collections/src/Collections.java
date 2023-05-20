05f3eafef38be28dd6768f00e5f1cbfadd28e43e
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-19 21:51:41 -0600

    Collections Structures

---
+++ Collections/src/Collections.java
@@ -0,0 +1,17 @@
+/**
+ * Las Collections son estructuras similares a los arreglos pero con la principal caracteristica de que son
+ * dinamicos (su tamaño y cantidad de elementos puede variar en el tiempo)
+ *
+ * En Java, las colecciones se emplean mediante la interfaz "Collection", que permite implementar una serie de
+ * metodos comunes como ser: Añadir, Eliminar, Obtener el tamaño de la colección, etc.
+ *
+ * Tipos principales de Collections:
+ *  + List
+ *  + Set
+ *  + Queue
+ *  + Map
+ *
+ */
+
+public class Collections {
+}
