60aac4790da28a9dcec881f9587cf93e9e4b57f5
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-20 01:13:38 -0600

    Collections - Lists - Stacks

---
+++ Collections/src/collections/StackCollection.java
@@ -0,0 +1,33 @@
+package collections;
+
+import java.util.Stack;
+
+public class StackCollection {
+
+  public static void main(String[] args) {
+
+    Stack<Integer> pila = new Stack<Integer>();
+
+    System.out.println("Lista vacía: " + pila);
+    System.out.println("Está vacía: " + pila.isEmpty());
+
+    // Agregar a la pila
+    pila.push(1);
+    pila.push(2);
+    pila.push(3);
+    pila.push(4);
+    pila.push(5);
+
+    // Recorrido
+    for (Integer pilita: pila
+         ) {
+      System.out.println(pilita);
+    }
+
+    // Mostrar
+    System.out.println("Pila: " + pila);
+    System.out.println("Pila vacía: " + pila.isEmpty());
+
+  }
+
+}
